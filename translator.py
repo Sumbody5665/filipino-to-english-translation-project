b9ae3b744bdc2870ecb55e9c00dbe71d6f694037
Sumbody5665 <racelisjethro@gmail.com>
2018-04-23 12:10:58 +0800

    translator now says how many sentences and words are loaded

--- translator.py
+++ translator.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 #from keras.models import Model
 #from keras.layers import Input, LSTM, Dense
 import numpy as np
@@ -6,7 +7,7 @@ import io
 #just a function to show how much of a loop is done already
 def show_loop_progress(counter,length):
 	if counter < length - 1 :
-		print("%.2f%%"%(100*counter/length) ,end="\r")
+		print("%.2f%%"%(100*counter/length) ,end='\r')
 	else:
 		print("%.2f%%"%(100*counter/length))
 	return
@@ -27,6 +28,7 @@ with open(filename,encoding="utf8") as f:
 #removes the start of file (not sure yet if that's what it is) character
 sentence_pairs[0] = sentence_pairs[0].strip("\ufeff")
 
+
 for x in range(len(sentence_pairs)):
 	pair = sentence_pairs[x]
 	pair = pair.replace(",", " , ")
@@ -39,6 +41,8 @@ for x in range(len(sentence_pairs)):
 	output = output + [[sos]+left.strip().split()+[eos]]
 	input = input + [[sos]+right.strip().split()+[eos]]
 	show_loop_progress(x,len(sentence_pairs))
+print("%s Filipino sentences and %s English sentences loaded"%(len(input),len(output)))
+
 
 print("Looking for maximum input sentence length...")
 input_max_word_count = 0
@@ -46,17 +50,23 @@ for x in range(len(input)):
 	if len(input[x]) > input_max_word_count:
 		input_max_word_count = len(input[x])
 	show_loop_progress(x,len(input))
-
 print("Looking for maximum output sentence length...")
 output_max_word_count = 0
 for x in range(len(output)):
 	if len(output[x]) > output_max_word_count:
 		output_max_word_count = len(output[x])
 	show_loop_progress(x,len(output))
-
 print("Maximum input sentence length is %s words"%input_max_word_count)
 print("Maximum output sentence length is %s words"%output_max_word_count)
-words_to_load = 100000
+
+
+words_to_load = -1
+#set to how many words you want to load
+#set to negative values if you want to load all words
+if words_to_load>0:
+	print("Words to load: %s"%(words_to_load))
+else: 
+	print("Words to load: all of them")
 print("Loading filipino word vectors...")
 fin = io.open('cc.tl.300.vec', 'r', encoding='utf-8', newline='\n', errors='ignore')
 n, word_dim = map(int, fin.readline().split())
@@ -65,9 +75,12 @@ x=0
 for line in fin:
 	tokens = line.rstrip().split(' ')
 	filipino[tokens[0]] = np.array(list(map(float, tokens[1:])))
-	show_loop_progress(x,words_to_load)
+	if words_to_load > 0 :
+		show_loop_progress(x,words_to_load)
+	else:
+		show_loop_progress(x,n)
 	x = x + 1
-	if x >= words_to_load:
+	if (x >= words_to_load) and (words_to_load > 0):
 		break
 filipino[sos] = np.zeros(word_dim)
 filipino[eos] = 0.5*np.ones(word_dim)
@@ -82,9 +95,12 @@ x=0
 for line in fin:
 	tokens = line.rstrip().split(' ')
 	english[tokens[0]] = np.array(list(map(float, tokens[1:])))
-	show_loop_progress(x,words_to_load)
+	if words_to_load > 0 :
+		show_loop_progress(x,words_to_load)
+	else:
+		show_loop_progress(x,n)
 	x = x + 1
-	if x >= words_to_load:
+	if (x >= words_to_load) and (words_to_load > 0):
 		break
 english[sos] = np.zeros(word_dim)
 english[eos] = 0.5*np.ones(word_dim)
@@ -116,11 +132,13 @@ for x in range(len(output)):
 			target_decoder_output[x][y] = english.get(output[x][y+1],english[oov])
 	show_loop_progress(x,len(output))
 
+print("Flipping encoder input...")
 encoder_input = np.flip(encoder_input,1)
-#encoder input has been flipped
+print("Encoder input has been flipped")
 
 print("encoder input shape: %s"% (encoder_input.shape,))
 print("target decoder input shape: %s"% (target_decoder_input.shape,))
 print("target decoder output shape: %s"% (target_decoder_output.shape,))
 print("auto decoder input shape: %s"% (auto_decoder_input.shape,))
 print("auto decoder output shape: %s"% (auto_decoder_output.shape,))
+
